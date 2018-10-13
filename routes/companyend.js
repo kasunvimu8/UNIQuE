08d164800d0dd5970d87411d14179e271bf7587f
Prabhath Bandara <okprabhath@gmail.com>
2018-10-13 17:56:42 +0530

    add boostrap new layout

--- routes/companyend.js
+++ routes/companyend.js
@@ -2,7 +2,21 @@ var express = require('express');
 var router = express.Router();
 var tapApi = require("tap-telco-api");
 
+router.get('/',function (req,res) {
+    res.render('index')
+})
 
+router.get('/dashboard',function (req,res) {
+    res.render('index')
+})
+
+router.get('/community',function (req,res) {
+    res.render('index')
+})
+
+router.get('/identify',function (req,res) {
+    res.render('index')
+})
 
 
 module.exports = router;
